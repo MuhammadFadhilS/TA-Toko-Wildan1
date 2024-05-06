2f86ad8ade9ec9e44a8edcd59d0a7bc3f02658b0
fadhil <ahmadkarim863@gmail.com>
2024-05-06 19:32:59 +0800

    semua tabel

---
+++ database/migrations/2024_05_06_111635_create_barangs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('gambar');
+            $table->string('harga');
+            $table->string('satuan');
+            $table->string('kode');
+            $table->text('deskripsi');
+            $table->string('diskon');
+            $table->string('stok');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
---
+++ database/migrations/2024_05_06_112136_create_penjualans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjualansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->string('penjualan_harian');
+            $table->string('penjualan_mingguan');
+            $table->string('penjualan_bulanan');
+            $table->string('penjualan_tahunan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjualans');
+    }
+}
